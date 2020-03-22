0ada34d5ab06ba5b45a19118d056e6cfdaa20793
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2020-03-22 16:56:05 +0700

    Updates

---
+++ precache-manifest.a4d3b4ba1277fa802f7b529eb58b5ae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7799a4dc061d752a5960ceb45c9b4904",
+    "url": "/nhom1-dacnpm/index.html"
+  },
+  {
+    "revision": "9b1105404d64192cee97",
+    "url": "/nhom1-dacnpm/static/js/2.6ca714d9.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/nhom1-dacnpm/static/js/2.6ca714d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dee50824d3681c5d92f",
+    "url": "/nhom1-dacnpm/static/js/main.8015249a.chunk.js"
+  },
+  {
+    "revision": "d68a5047326ecf8011e8",
+    "url": "/nhom1-dacnpm/static/js/runtime-main.1443f721.js"
+  }
+]);
