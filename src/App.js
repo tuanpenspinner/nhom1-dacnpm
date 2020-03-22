cf2d09750eb1a8bbd48440a2a422517ebb195e6b
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2020-03-22 17:41:51 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Box from "./components/game/Box";
 import Game from "./components/admin/Game";
