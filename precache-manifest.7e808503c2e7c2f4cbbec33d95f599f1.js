92a849cff9a10dd3c1dce66c9af15529788bb6a8
Nguyễn Thanh Tuấn <tuanpenspinner@gmail.com>
2020-04-12 12:47:44 +0700

    Updates

--- precache-manifest.7e808503c2e7c2f4cbbec33d95f599f1.js
+++ precache-manifest.7e808503c2e7c2f4cbbec33d95f599f1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79e7090e7ad44a84b0bf3500fbaa8437",
+    "revision": "e5f28cdf93f564a390a05ea130da91ad",
     "url": "/nhom1-dacnpm/index.html"
   },
   {
-    "revision": "59d9d6895fcb78747d9e",
-    "url": "/nhom1-dacnpm/static/css/main.98754e36.chunk.css"
+    "revision": "83a67e421337d12adb8f",
+    "url": "/nhom1-dacnpm/static/css/main.040138ff.chunk.css"
   },
   {
-    "revision": "b83075769b18be2f648b",
-    "url": "/nhom1-dacnpm/static/js/2.495c88c8.chunk.js"
+    "revision": "11af375b35845651698a",
+    "url": "/nhom1-dacnpm/static/js/2.c393a014.chunk.js"
   },
   {
     "revision": "5c2176e7db1837929debf21c65634f23",
-    "url": "/nhom1-dacnpm/static/js/2.495c88c8.chunk.js.LICENSE.txt"
+    "url": "/nhom1-dacnpm/static/js/2.c393a014.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59d9d6895fcb78747d9e",
-    "url": "/nhom1-dacnpm/static/js/main.f3fddc16.chunk.js"
+    "revision": "83a67e421337d12adb8f",
+    "url": "/nhom1-dacnpm/static/js/main.d6cac994.chunk.js"
   },
   {
     "revision": "d68a5047326ecf8011e8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nhom1-dacnpm/precache-manifest.2a4e9be53cc5b537ad250563ebcd6415.js"
+  "/nhom1-dacnpm/precache-manifest.7e808503c2e7c2f4cbbec33d95f599f1.js"
 );
 
 self.addEventListener('message', (event) => {
