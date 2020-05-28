6021883bb40b289ee01a24a090de6697bf5d2c12
Tuan <tuanpenspinnner@gmail.com>
2020-05-28 11:14:16 +0700

    Updates

--- precache-manifest.fadf7498efa0939bb2838cd5c14854c5.js
+++ precache-manifest.fadf7498efa0939bb2838cd5c14854c5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "382230d546336696148a197a047f4238",
+    "revision": "75ccd7c1bea6e0ed5dd84b600bd4e113",
     "url": "/nhom1-dacnpm/index.html"
   },
   {
-    "revision": "d130e3233f2782fc4869",
-    "url": "/nhom1-dacnpm/static/css/main.b3fdd304.chunk.css"
+    "revision": "540943c5fe09d61e3487",
+    "url": "/nhom1-dacnpm/static/css/main.32365f1f.chunk.css"
   },
   {
     "revision": "90c0dd7b49e7fb4dfd1a",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nhom1-dacnpm/static/js/2.e31b4d99.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d130e3233f2782fc4869",
-    "url": "/nhom1-dacnpm/static/js/main.fbe35c83.chunk.js"
+    "revision": "540943c5fe09d61e3487",
+    "url": "/nhom1-dacnpm/static/js/main.99f05b04.chunk.js"
   },
   {
     "revision": "d68a5047326ecf8011e8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nhom1-dacnpm/precache-manifest.055547bda94adf42be44c2155d4cefb2.js"
+  "/nhom1-dacnpm/precache-manifest.fadf7498efa0939bb2838cd5c14854c5.js"
 );
 
 self.addEventListener('message', (event) => {
